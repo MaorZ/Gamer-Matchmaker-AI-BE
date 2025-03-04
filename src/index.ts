8b9ae8956f729022d49faea11bcbfe29f494f1ca
MaorZ <maorz1@gmail.com>
2025-03-05 00:02:36 +0200

    inital node + ts + langchain commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { OpenAI } from '@langchain/openai';
+import * as dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+async function main() {
+  console.log('wow!');
+}
+
+main().catch(console.error);
